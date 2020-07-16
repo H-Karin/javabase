9dbf27151719e260c8d6490581a7c0cd07768dd9
H-Karin <hkn.rrn@gmail.com>
2020-07-16 13:10:14 +0900

    挨拶を変更

--- testGit/src/sample/Main.java
+++ testGit/src/sample/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello GitHub!");
-		System.out.println("繋がったよ！");
-		System.out.println("やったね！");
+		System.out.println("hello GitHub!");
+		System.out.println("hello GitHub!");
+		System.out.println("hello GitHub!");
+		System.out.println("hello GitHub!");
 	}
 
 }
